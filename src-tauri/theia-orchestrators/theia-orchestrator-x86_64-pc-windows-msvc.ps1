d63a0d70b65a10414a50770354b94493b3dc0112
tensor-programming <tensordeveloper@gmail.com>
2020-05-25 20:24:45 -0400

    update script

--- src-tauri/theia-orchestrators/theia-orchestrator-x86_64-pc-windows-msvc.ps1
+++ src-tauri/theia-orchestrators/theia-orchestrator-x86_64-pc-windows-msvc.ps1
@@ -4,36 +4,49 @@
 $mode = $args[0]
 $path = $args[1]
 
-# Names of the associated proceesses
+# Names of the associated processes
 $wwa_host = "WWAHost"
 $webview_proc = "Win32WebViewHost"
-$app_name = "theia-x68_64-pc-windows-msvc"
+
+# Get a list of process objects
+$procs = Get-Process $wwa_host, $webview_proc | Format-List *
 
 # allow loopback
 CheckNetIsolation.exe LoopbackExempt -a -n="Microsoft.Win32WebViewHost_cw5n1h2txyewy"
 
 # if mode is run
 if ($mode -eq "run") {
-    # start process on the inputted filepath. 
-    Start-Process -FilePath $path -PassThru -NoNewWindow 
+    # start main process. 
+    $main_proc = Start-Process -FilePath $path -PassThru -NoNewWindow -Wait
+    # Wait for the process to exit before continuing. 
+    $main_proc.WaitForExit()
 
-    # get the main process and wait until that process exits before execution. 
-    $main_proc = Get-Process -Name $app_name -ErrorAction SilentlyContinue | Wait-Process 
+    # Iterate through the process list. 
+    foreach ($proc in $procs) {
+        # shutdown each process. 
+        Shutdown_Proc($name)
+    }
+    
+    # Make sure that the main process has exited
+    if (-Not $main_proc.HasExited) {
+        Shutdown_Proc($main_proc)
+    }
 
-    # stop the main process 
-    Stop-Process -InputObject $main_proc
-
-    # get the proccesses associated with the webview and wwa. 
-    $wv_proc = Get-Process -Name $webview_proc
-    $wwa_proc = Get-Process -Name $wwa_host
-
-    # Kill WWA and Webview
-    $wv_proc.Kill()
-    $wwa_proc.Kill()
-
-    # check that all processes associated with objects have exited. 
+    # check that all processes have exited. 
     Get-Process | Where-Object { $_.HasExited }
-  
+
     # kill this porcess. 
     Stop-process -Id $PID
+}
+
+# Simple function to shutdown processes without crashing the script
+function Shutdown_Proc(
+    [Object]$proc
+) {
+    Try {
+        Stop-Process -InputObject $proc -Force
+    }
+    Catch [System.NullReferenceException] {
+        Write-Output $_.Exception | Format-List -Force
+    }
 }
